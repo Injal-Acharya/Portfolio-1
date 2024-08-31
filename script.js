1a5c524a786eff5a8c188a90639f66b9097d767f
AI-3Tech <injalacharya1@gmail.com>
2024-08-31 21:15:17 +0545

    Scrolling and content updated

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
             //active navbar links
 
+            
             navlinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
